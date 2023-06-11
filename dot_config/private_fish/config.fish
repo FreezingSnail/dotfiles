4e622fd8fde017d4ca43eacea2e5cb6c5e45402a
FreezingSnail <smogonv3@gmail.com>
2023-06-11 16:09:26 -0400

    fish configs

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,3 +1,8 @@
 if status is-interactive
 	eval (zellij setup --generate-auto-start fish | string collect)
 end
+
+
+set fzf_configure_bindings --directory\cf --variables=\e\cv
+set fzf_configure_bindings --git_status=\cg --history=\ch --variables= --processes=
+set fzf_preview_dir_cmd exa --all --color=always`
